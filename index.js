9b7d9513955193742bcc9da8945eddadefef7d9d
tj-commits <thefoxjones@gmail.com>
2025-12-17 09:34:13 -0600

    update

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-require('vanilla-javascript') // because we are making something awesome!
-require('none')() // for performance
-
 function hasSelfEquality(value) {
-  return value === value // === checks for both value and type equality
+  return value === value
 }
 
 module.exports = hasSelfEquality
